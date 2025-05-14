3127cc9134c20c82deb4bb5a5ca757fc2c96ee2b
bkawecki <121129764+bjkawecki@users.noreply.github.com>
2025-05-14 10:34:51 +0200

    debug quart server conditional start

--- terraform/main.tf
+++ terraform/main.tf
@@ -69,6 +69,7 @@ resource "aws_instance" "public" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   user_data              = file("user_data.sh")
   key_name               = aws_key_pair.my_key.key_name
+  count                  = 1
 
 
   tags = {
